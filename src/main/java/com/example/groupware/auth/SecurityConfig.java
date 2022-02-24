77f305efc11050bd88b126a7111f64f93fa352cc
shbaek <shbaek@yettiesoft.com>
2022-02-24 17:30:21 +0900

    #comment 안쓰는 클래스 수정

--- src/main/java/com/example/groupware/auth/SecurityConfig.java
+++ src/main/java/com/example/groupware/auth/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.groupware.auth;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
  
